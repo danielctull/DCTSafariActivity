d233459e16acfd82f93226574951cc80c0666eab
Daniel Tull <dt@danieltull.co.uk>
2014-08-23 22:20:41 +0100

    Add assets and version info

--- DCTSafariActivity/DCTSafariActivity.h
+++ DCTSafariActivity/DCTSafariActivity.h
@@ -8,5 +8,9 @@
 
 @import UIKit;
 
+//! Project version number and string for DCTSafariActivity.
+FOUNDATION_EXPORT double DCTSafariActivityVersionNumber;
+FOUNDATION_EXPORT const unsigned char DCTSafariActivityVersionString[];
+
 @interface DCTSafariActivity : UIActivity
 @end
